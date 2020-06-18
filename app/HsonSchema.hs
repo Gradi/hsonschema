6409509d2c2699938021890f27a47d69da18370f
Maksim Kislyakov <mail@kmaksim.dev>
2020-06-18 09:27:11 +0300

    Fix issue with generating schema for every array value instead of unique value types.

--- app/HsonSchema.hs
+++ app/HsonSchema.hs
@@ -7,6 +7,8 @@ import qualified Data.Aeson as Json
 import qualified Data.HashMap.Strict as M
 import qualified Data.Text as T
 import qualified Data.Vector as V
+import Data.Data(toConstr)
+import Data.List(groupBy)
 
 type Schema = Json.Object
 
@@ -17,10 +19,17 @@ makeSchema Json.Null     = newSchema JNull
 makeSchema (Json.Bool   _) = newSchema JBoolean
 makeSchema (Json.Number _) = newSchema JNumber
 makeSchema (Json.String _) = newSchema JString
-makeSchema (Json.Array  arr) =
-    newSchema JArray
-    `withProp` ("length", V.length arr)
-    `withProp` ("items", Json.Array $ fmap (Json.Object . makeSchema) arr)
+makeSchema (Json.Array  arr) = 
+    let
+        uniqueArr = uniqueByTypes arr
+        uniqueLength = V.length uniqueArr
+        items = fmap (Json.Object . makeSchema) $ uniqueArr
+        schema = newSchema JArray `withProp` ("length", V.length arr)
+    in
+        schema `withProp` ("items", case uniqueLength of
+            0 -> Json.Object emptyObject
+            1 -> V.head items
+            _ -> Json.Array items)
 makeSchema (Json.Object map) =
     newSchema JObject
     `withProp` ("size", M.size map)
@@ -34,6 +43,17 @@ obj `withProp` (key, val) = M.insert (T.pack key) (Json.toJSON val) obj
 
 newSchema :: JType -> Schema
 newSchema t = emptyObject `withProp` ("type", jTypeToStr t)
+
+uniqueByTypes :: Json.Array -> Json.Array
+uniqueByTypes = V.fromList . ungroupHeads . (groupBy typeEqual) . V.toList
+
+typeEqual :: Json.Value -> Json.Value -> Bool
+typeEqual a b = toConstr a == toConstr b
+
+ungroupHeads :: [[a]] -> [a]
+ungroupHeads [] = []
+ungroupHeads ([]:xs) = ungroupHeads xs
+ungroupHeads ((x:xs):ys) = x : ungroupHeads ys
 
 jTypeToStr :: JType -> String
 jTypeToStr JString  = "string"
